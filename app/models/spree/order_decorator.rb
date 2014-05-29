6386a0f6abe5da8ec3f5a9d70f2e71fa2cffa1ae
Jason King <herqueles3@gmail.com>
2014-05-29 00:26:05 -0400

    changes for updated spree

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -63,7 +63,7 @@ Spree::Order.class_eval do
     logger.debug rtn_tax
     adjustments.create do |adjustment|
       adjustment.source = self
-      adjustment.originator = avalara_transaction
+      adjustment.adjustable = avalara_transaction
       adjustment.label = 'Tax'
       adjustment.mandatory = true
       adjustment.eligible = true
@@ -90,7 +90,7 @@ Spree::Order.class_eval do
       logger.debug rtn_tax
       adjustments.create do |adjustment|
         adjustment.source = self
-        adjustment.originator = avalara_transaction
+        adjustment.adjustable = avalara_transaction
         adjustment.label = 'Tax'
         adjustment.mandatory = true
         adjustment.eligible = true
